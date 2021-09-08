9cc4f1fdec7a16dd10e14a150eac15d10ebd0a3e
Sneha2611 <snehaacharya26@yahoo.in>
2021-09-08 17:16:51 +0530

    latest commit

--- src/main/java/com/flipkart/qa/base/TestBase.java
+++ src/main/java/com/flipkart/qa/base/TestBase.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-//iiiiiiiiiiiiiii
+
 
 public class TestBase {
 
